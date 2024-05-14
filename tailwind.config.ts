18ff896b297356c915a7ec12009934b3c84931d5
robMolloy <robmolloy@hotmail.co.uk>
2024-05-14 11:58:46 +0100

    tailwind typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {},
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: { themes: true },
 };
 export default config;
