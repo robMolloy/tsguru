9afa2d5ee12a95049e4144fdc7319c436c55f907
robMolloy <robmolloy@hotmail.co.uk>
2024-05-16 17:16:57 +0100

    title and use highlight js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,19 @@ import { Layout } from "@/modules/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import Head from "next/head";
+
+import "highlight.js/styles/default.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>TS Gurus</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
