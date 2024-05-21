0525a1ec2729def73ce90614083921458c4f0743
robMolloy <robmolloy@hotmail.co.uk>
2024-05-21 11:33:14 +0100

    unsuccessful noInfer test

---
+++ src/tests/noInferTest.ts
@@ -0,0 +1,10 @@
+// // type a = {
+// //   a: "hi";
+// //   b: "bye";
+// // };
+// // type b = Pick<a, "b">;
+// function createStreetLight<C extends string>(colors: C[], defaultColor?: NoInfer<C>) {
+//   // ...
+// }
+
+// createStreetLight(["red", "yellow", "green"], "blue");
