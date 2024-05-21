8954870acf4f90803214f22a4acdc7141b360019
robMolloy <robmolloy@hotmail.co.uk>
2024-05-21 11:47:36 +0100

    new pages - setup

---
+++ src/pages/articles/no-infer-article.page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>no-infer-article.page</h1>
+    </div>
+  );
+}
---
+++ src/pages/recommendations/run-time-checking-recommendation.page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>run-time-checking Recommendations</h1>
+    </div>
+  );
+}
