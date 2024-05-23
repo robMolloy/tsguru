6b571c4beb442832b45a2650b44cdea118902ec4
robMolloy <robmolloy@hotmail.co.uk>
2024-05-23 17:42:36 +0100

    sidemenu active improvement

--- src/modules/layout/SideMenu.tsx
+++ src/modules/layout/SideMenu.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Router from "next/router";
 
 type TLinksMapLink = { type: "link"; href: string; label: string; c?: true };
 type TLinksMapLinkGroup = {
@@ -31,15 +32,16 @@ const linksMap: TLinksMap = [
     href: "/guides",
     label: "Guides",
     links: [
+      { type: "link", href: "/certainty-boundary", label: "certainty-boundary" },
       { type: "link", href: "/data-consumption", label: "data-consumption" },
       { type: "link", href: "/discriminated-unions", label: "discriminated-unions" },
+      { type: "link", href: "/fetching-data-safely-react", label: "Fetching Data Safely In React" },
       { type: "link", href: "/generics-extend-explain", label: "generics-extend-explain" },
       { type: "link", href: "/generics-extend-visual", label: "generics-extend-visual" },
       { type: "link", href: "/infer-types-from-vars", label: "infer-types-from-vars" },
       { type: "link", href: "/intro-to-generics", label: "intro-to-generics" },
       { type: "link", href: "/name-your-data-types-well", label: "name-your-data-types-well" },
       { type: "link", href: "/never-assert", label: "never-assert" },
-      { type: "link", href: "/safe-fetch-fe", label: "safe-fetch-fe" },
       { type: "link", href: "/smart-dumb-tight-ui", label: "smart-dumb-tight-ui" },
       { type: "link", href: "/tight-vs-loose-types", label: "tight-vs-loose-types" },
       { type: "link", href: "/ts-encourages-better", label: "ts-encourages-better" },
@@ -53,15 +55,20 @@ const linksMap: TLinksMap = [
     href: "/recommendations",
     label: "Recommendations",
     links: [
-      { type: "link", href: "/development-workflow", label: "Development Workflow", c: true },
-      { type: "link", href: "/framework", label: "framework recommendation" },
+      {
+        type: "link",
+        href: "/development-workflow",
+        label: "Development Workflow Recommendation",
+        c: true,
+      },
+      { type: "link", href: "/framework", label: "Framework Recommendation", c: true },
       {
         type: "link",
         href: "/run-time-checking",
-        label: "run-time checking recommendation",
+        label: "Checking at run-time recommendation",
         c: true,
       },
-      { type: "link", href: "/tsconfig", label: "tsconfig recommendation" },
+      { type: "link", href: "/tsconfig", label: "tsconfig Recommendation" },
     ],
   },
   {
@@ -94,7 +101,7 @@ export const SideMenu = (p: { show: boolean }) => {
           if (item.type === "link")
             return (
               <li key={item.href}>
-                <Link href={item.href}>
+                <Link href={item.href} className={Router.route === item.href ? "active" : ""}>
                   {item.label} {item.c ? "!!!COMPLETE!!!" : ""}
                 </Link>
               </li>
@@ -105,12 +112,20 @@ export const SideMenu = (p: { show: boolean }) => {
               <li key={item.href}>
                 <details open>
                   <summary>
-                    <Link href={item.href}>{item.label}</Link>
+                    <Link
+                      href={item.href}
+                      className={Router.route === `${item.href}` ? "active" : ""}
+                    >
+                      {item.label}
+                    </Link>
                   </summary>
                   <ul>
                     {item.links.map((child) => (
-                      <li key={`${item.href}${child.href}`}>
-                        <Link href={`${item.href}${child.href}`}>
+                      <li key={`${item.href}${child.href}`} className="active">
+                        <Link
+                          href={`${item.href}${child.href}`}
+                          className={Router.route === `${item.href}${child.href}` ? "active" : ""}
+                        >
                           {child.label} {child.c ? "!!!COMPLETE!!!" : ""}
                         </Link>
                       </li>
