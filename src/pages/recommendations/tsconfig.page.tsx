ccad778f8ed2d28bc0049d2c0e7a9d89472c48be
robMolloy <robmolloy@hotmail.co.uk>
2024-05-24 14:13:02 +0100

    tsconfig.page update

--- src/pages/recommendations/tsconfig.page.tsx
+++ src/pages/recommendations/tsconfig.page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
       </p>
       <p>
         Learn more about how the compiler can help when you're writing code by reviewing{" "}
-        <Link href="/recommendations/development-workflow">TS Development Workflow</Link>
+        <Link href="/recommendations/development-workflow">TS Development Workflow</Link>.
       </p>
 
       <h2>Initial setup</h2>
@@ -44,32 +44,33 @@ export default function Page() {
 `}</CodeBlock>
       <p>This is an acceptable basic setup but let's make a few changes.</p>
 
-      <h2>"allowJs": true,</h2>
+      <h2>"allowJs": true</h2>
       <p>
         It's unlikely that developers will start writing JavaScript in a TypeScript project, but why
-        take the risk? Let's delete <code>"allowJs": true,</code> and this will default to false.
+        take the risk? Let's delete <code>"allowJs": true</code> and this will default to false.
       </p>
 
-      <h2>"noUncheckedIndexedAccess": true,</h2>
+      <h2>"noUncheckedIndexedAccess": true</h2>
       <p>
         This will help catch potential undefined errors when accessing object properties. For more
         info read this article where we go into more detail{" "}
         <Link href="/articles/no-unchecked-indexed-access">tsconfig: noUncheckedIndexedAccess</Link>
+        .
       </p>
-      <h2>"strict": true,</h2>
+      <h2>"strict": true</h2>
       <p>
-        <code>"strict": true,</code> enforces a set of helpful and sensible rules. These are easy to
+        <code>"strict": true</code> enforces a set of helpful and sensible rules. These are easy to
         conform to from the start of the project if you have a good understanding of TypeScript.
         However, the migration process is a lot more effort once a many lines of code have been
         written that break this set of rules.
       </p>
 
-      <h2>"noImplicitAny": false,</h2>
+      <h2>"noImplicitAny": false</h2>
       <p>
-        Please please please (let me get what I want), don't add{" "}
-        <code>"noImplicitAny": false,</code> to your config. This is one of the major ways that the
-        typing system can be completely undermined by just a small amount of code. There really
-        shouldn't be any <code>any</code>s in your codebase if you value the benefits of TypeScript.
+        Please please please (let me get what I want), don't add <code>"noImplicitAny": false</code>{" "}
+        to your config. This is one of the major ways that the typing system can be completely
+        undermined by just a small amount of code. There really shouldn't be any <code>any</code>s
+        in your codebase if you value the benefits of TypeScript.
       </p>
 
       <h2>Be careful...</h2>
@@ -88,7 +89,7 @@ export default function Page() {
       <h2>Finally</h2>
       <p>
         Don't forget to read about our recommended workflow as reference in the intro{" "}
-        <Link href="/recommendations/development-workflow">TS Development Workflow</Link>
+        <Link href="/recommendations/development-workflow">TS Development Workflow</Link>.
       </p>
     </Typography>
   );
