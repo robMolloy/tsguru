285d049e05d6b92abd0fc5a9c3e98a4500c78553
robMolloy <robmolloy@hotmail.co.uk>
2024-06-03 00:51:58 +0100

    advise service

--- src/pages/services/advise-service.page.tsx
+++ src/pages/services/advise-service.page.tsx
@@ -3,6 +3,37 @@ export default function Page() {
   return (
     <Typography fullPage>
       <h1>Advise Service</h1>
+      <h2>What we do</h2>
+      <p>
+        Our advisory service caters to a limited number of teams and partner businesses, offering
+        on-demand support and guidance. We specialize in providing comprehensive advice and
+        recommendations on TypeScript best practices tailored to meet the unique needs of each
+        client.
+      </p>
+
+      <h2>Expert Guidance</h2>
+      <p>
+        Our team of experts provides valuable insights and recommendations based on industry best
+        practices and extensive experience. This guidance helps teams make informed decisions, avoid
+        common pitfalls in TypeScript development and receive advice on particular problems they
+        face.
+      </p>
+
+      <h2>Continuous Improvement</h2>
+      <p>
+        Our advisory service fosters a culture of continuous improvement within teams by providing
+        ongoing support and updates on the latest developments in TypeScript. This enables teams to
+        adopt many of the best-practices that are not always easy to adopt in one sweeping change
+        but instead can be added to a project incrementally with the help of our advisory service.
+      </p>
+
+      <h2>Regular advice</h2>
+      <p>
+        Upon request, we offer regularly scheduled one-on-one or group consultations aimed at
+        enhancing developers' understanding of TypeScript and their ability to write high-quality
+        code. Additionally, we can participate in code reviews and other feedback processes to
+        further support development teams.
+      </p>
     </Typography>
   );
 }
