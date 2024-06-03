1a31518a7b2e981a36a6372112605da7f4a79531
robMolloy <robmolloy@hotmail.co.uk>
2024-06-03 16:37:24 +0100

    deploy-check

--- src/tests/noInferTest.ts
+++ src/tests/noInferTest.ts
@@ -1,39 +1,39 @@
-type NoInfer<T> = T & { [K in keyof T]: T[K] };
+// type NoInfer<T> = T & { [K in keyof T]: T[K] };
 
-function someFn(x: { allPossibleValues: string[]; value: string }) {
-  // ...
-}
-someFn({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
+// function someFn(x: { allPossibleValues: string[]; value: string }) {
+//   // ...
+// }
+// someFn({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
 
-function someFn1<T extends { allPossibleValues: string[]; value: string }>(x: T) {
-  // ...
-}
-const allPossibleValues1 = ["hi", "bye", "cry"] as ("hi" | "bye" | "cry")[];
-type TParam1 = {
-  allPossibleValues: typeof allPossibleValues1;
-  value: NonNullable<(typeof allPossibleValues1)[number]>;
-};
-// @ts-expect-error
-someFn1<TParam1>({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
+// function someFn1<T extends { allPossibleValues: string[]; value: string }>(x: T) {
+//   // ...
+// }
+// const allPossibleValues1 = ["hi", "bye", "cry"] as ("hi" | "bye" | "cry")[];
+// type TParam1 = {
+//   allPossibleValues: typeof allPossibleValues1;
+//   value: NonNullable<(typeof allPossibleValues1)[number]>;
+// };
+// // @ts-expect-error
+// someFn1<TParam1>({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
 
-function someFn2<T1 extends string, T2 extends T1>(x: { allPossibleValues: T1[]; value: T2 }) {
-  // ...
-}
-// @ts-expect-error
-someFn2({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
+// function someFn2<T1 extends string, T2 extends T1>(x: { allPossibleValues: T1[]; value: T2 }) {
+//   // ...
+// }
+// // @ts-expect-error
+// someFn2({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
 
-function someFn3<T1 extends string>(x: { allPossibleValues: T1[]; value: NoInfer<T1> }) {
-  /*logic here*/
-}
-// @ts-expect-error
-someFn3({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
+// function someFn3<T1 extends string>(x: { allPossibleValues: T1[]; value: NoInfer<T1> }) {
+//   /*logic here*/
+// }
+// // @ts-expect-error
+// someFn3({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour" });
 
-function someFn4<T1 extends string>(x: {
-  allPossibleValues: T1[];
-  value: NoInfer<T1>;
-  value2: NoInfer<T1>;
-}) {
-  /*logic here*/
-}
-// @ts-expect-error
-someFn4({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour", value2: "olah" });
+// function someFn4<T1 extends string>(x: {
+//   allPossibleValues: T1[];
+//   value: NoInfer<T1>;
+//   value2: NoInfer<T1>;
+// }) {
+//   /*logic here*/
+// }
+// // @ts-expect-error
+// someFn4({ allPossibleValues: ["hi", "bye", "cry"], value: "bonjour", value2: "olah" });
