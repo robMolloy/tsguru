f5a14287207f3fd33dc292eb6080c178659a2501
robMolloy <robmolloy@hotmail.co.uk>
2024-06-05 12:44:52 +0100

    add scrollBehavior: "smooth"

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="aqua">
+    <Html lang="en" data-theme="aqua" style={{ scrollBehavior: "smooth" }}>
       <Head />
       <body>
         <Main />
