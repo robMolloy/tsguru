9f4ae406f6bd027b0c532c5bcdff1043c2fc7378
robMolloy <robmolloy@hotmail.co.uk>
2024-06-05 17:33:04 +0100

    create new services setup - learn,play,teach

--- src/pages/services/play-service.page.tsx
+++ src/pages/services/play-service.page.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@/components";
 export default function Page() {
   return (
     <Typography fullPage>
-      <h1>Play service</h1>
+      <h1>Play Service</h1>
       <h2>This service is coming soon, please check back later...</h2>
     </Typography>
   );
--- src/pages/services/teach-service.page.tsx
+++ src/pages/services/teach-service.page.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@/components";
 export default function Page() {
   return (
     <Typography fullPage>
-      <h1>Learn Service</h1>
+      <h1>Teach Service</h1>
       <h2>This service is coming soon, please check back later...</h2>
     </Typography>
   );
