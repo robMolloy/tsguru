2197e8a77fae3a8071017394a1f69e7040530fb7
robMolloy <robmolloy@hotmail.co.uk>
2024-06-05 12:43:08 +0100

    footer added to layout

--- src/modules/layout/Layout.tsx
+++ src/modules/layout/Layout.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { NavigationTree, linksMap } from "@/modules/NavigationTree";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Footer, Typography } from "@/components";
+import { GetInTouchForm } from "../getInTouchForm";
 
 const CloseDrawerWrapper: React.FC<{ children?: React.ReactNode }> = (p) => {
   return (
@@ -21,7 +23,7 @@ const OpenDrawerWrapper: React.FC<{ children?: React.ReactNode }> = (p) => {
 const NavBarContainer = (p: { children: React.ReactNode }) => {
   return (
     <div className="sticky top-0 z-[98]">
-      <div className="w-full navbar bg-base-300 border-b">{p.children}</div>
+      <div className="navbar w-full border-b bg-base-300">{p.children}</div>
     </div>
   );
 };
@@ -31,7 +33,7 @@ const BurgerMenuIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    className="inline-block w-6 h-6 stroke-current"
+    className="inline-block h-6 w-6 stroke-current"
   >
     <path
       strokeLinecap="round"
@@ -44,13 +46,13 @@ const BurgerMenuIcon = () => (
 
 const NavBarDropdown = (p: { children: React.ReactNode; label: string }) => {
   return (
-    <div className="dropdown dropdown-start dropdown-bottom">
+    <div className="dropdown-start dropdown dropdown-bottom">
       <div tabIndex={0} role="button" className="btn btn-ghost">
         <div>{p.label} &#x25BC;</div>
       </div>
       <div
         tabIndex={0}
-        className="dropdown-content mt-1 z-[1] p-0 shadow bg-base-100 rounded-box border"
+        className="dropdown-content z-[1] mt-1 rounded-box border bg-base-100 p-0 shadow"
         style={{ opacity: "0.94" }}
       >
         <div className="max-h-[75vh] overflow-y-scroll rounded-box">{p.children}</div>
@@ -74,105 +76,120 @@ export const Layout = (p: { children: React.ReactNode }) => {
   const { show: showTabsInBar } = useFlicker(router.route);
 
   return (
-    <div className="drawer">
-      <input id="sidebar" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        {/* Navbar */}
-        <NavBarContainer>
-          <div className="flex flex-col w-full">
-            <div className="flex w-full">
-              <div className="block sm:hidden">
-                <OpenDrawerWrapper>
-                  <BurgerMenuIcon />
-                </OpenDrawerWrapper>
-              </div>
+    <>
+      <div className="drawer">
+        <input id="sidebar" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <NavBarContainer>
+            <div className="flex w-full flex-col">
+              <div className="flex w-full">
+                <div className="block sm:hidden">
+                  <OpenDrawerWrapper>
+                    <BurgerMenuIcon />
+                  </OpenDrawerWrapper>
+                </div>
 
-              <div className="flex-1">
-                <Link href="/" className="text-xl btn btn-ghost">
-                  TS Gurus
-                </Link>
-              </div>
-              <div className="flex gap-4">
-                <div className="hidden sm:block">
-                  <Link href="/services" className="btn btn-ghost">
-                    View Services
+                <div className="flex-1">
+                  <Link href="/" className="btn btn-ghost text-xl">
+                    TS Gurus
                   </Link>
                 </div>
-                <Link href="#get-in-touch-form" className="btn btn-primary ">
-                  Get in touch
-                </Link>
+                <div className="flex gap-4">
+                  <div className="hidden sm:block">
+                    <Link href="/services" className="btn btn-ghost">
+                      View Services
+                    </Link>
+                  </div>
+                  <Link href="#get-in-touch-form" className="btn btn-primary">
+                    Get in touch
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="hidden sm:flex w-full">
-              {showTabsInBar && (
-                <>
-                  <NavBarDropdown label="Services">
-                    <NavigationTree
-                      linksMap={(() => {
-                        const links = linksMap.find(
-                          (x) => x.href === "/services" && x.type === "linkGroup"
-                        );
+              <div className="hidden w-full sm:flex">
+                {showTabsInBar && (
+                  <>
+                    <NavBarDropdown label="Services">
+                      <NavigationTree
+                        linksMap={(() => {
+                          const links = linksMap.find(
+                            (x) => x.href === "/services" && x.type === "linkGroup",
+                          );
 
-                        return [links as NonNullable<typeof links>];
-                      })()}
-                      type="submenu"
-                    />
-                  </NavBarDropdown>
-                  <NavBarDropdown label="Articles">
-                    <NavigationTree
-                      linksMap={(() => {
-                        const links = linksMap.find(
-                          (x) => x.href === "/articles" && x.type === "linkGroup"
-                        );
-                        return [links as NonNullable<typeof links>];
-                      })()}
-                      type="submenu"
-                    />
-                  </NavBarDropdown>
-                  <NavBarDropdown label="Guides">
-                    <NavigationTree
-                      linksMap={(() => {
-                        const links = linksMap.find(
-                          (x) => x.href === "/guides" && x.type === "linkGroup"
-                        );
-                        return [links as NonNullable<typeof links>];
-                      })()}
-                      type="submenu"
-                    />
-                  </NavBarDropdown>
-                  <NavBarDropdown label="Recommendations">
-                    <NavigationTree
-                      linksMap={(() => {
-                        const links = linksMap.find(
-                          (x) => x.href === "/recommendations" && x.type === "linkGroup"
-                        );
-                        return [links as NonNullable<typeof links>];
-                      })()}
-                      type="submenu"
-                    />
-                  </NavBarDropdown>
-                  {/* <NavBarDropdown label="Themes">
+                          return [links as NonNullable<typeof links>];
+                        })()}
+                        type="submenu"
+                      />
+                    </NavBarDropdown>
+                    <NavBarDropdown label="Articles">
+                      <NavigationTree
+                        linksMap={(() => {
+                          const links = linksMap.find(
+                            (x) => x.href === "/articles" && x.type === "linkGroup",
+                          );
+                          return [links as NonNullable<typeof links>];
+                        })()}
+                        type="submenu"
+                      />
+                    </NavBarDropdown>
+                    <NavBarDropdown label="Guides">
+                      <NavigationTree
+                        linksMap={(() => {
+                          const links = linksMap.find(
+                            (x) => x.href === "/guides" && x.type === "linkGroup",
+                          );
+                          return [links as NonNullable<typeof links>];
+                        })()}
+                        type="submenu"
+                      />
+                    </NavBarDropdown>
+                    <NavBarDropdown label="Recommendations">
+                      <NavigationTree
+                        linksMap={(() => {
+                          const links = linksMap.find(
+                            (x) => x.href === "/recommendations" && x.type === "linkGroup",
+                          );
+                          return [links as NonNullable<typeof links>];
+                        })()}
+                        type="submenu"
+                      />
+                    </NavBarDropdown>
+                    {/* <NavBarDropdown label="Themes">
                     <div className="p-2">
                       <div className="overflow-scroll">
                         <ThemeSelector />
                       </div>
                     </div>
                   </NavBarDropdown> */}
-                </>
-              )}
-              <button className="opacity-0 btn" disabled></button>
+                  </>
+                )}
+                <button className="btn opacity-0" disabled></button>
+              </div>
             </div>
-          </div>
-        </NavBarContainer>
-        {p.children}
-      </div>
-      <div className="drawer-side z-[99]">
-        <CloseDrawerWrapper />
+          </NavBarContainer>
+          {p.children}
+        </div>
+        <div className="drawer-side z-[99]">
+          <CloseDrawerWrapper />
 
-        <div className="p-1 m-0 min-w-80 min-h-full bg-base-100 border-r">
-          <NavigationTree linksMap={linksMap} ListWrapper={CloseDrawerWrapper} />
+          <div className="m-0 min-h-full min-w-80 border-r bg-base-100 p-1">
+            <NavigationTree linksMap={linksMap} ListWrapper={CloseDrawerWrapper} />
+          </div>
         </div>
       </div>
-    </div>
+      <Footer>
+        <Typography>
+          <h2>Get in touch!</h2>
+          <p>
+            We welcome all enquiries! For a detailed discussion on how we can tailor our services to
+            your unique situation, please contact us directly and one of our experts will get back
+            to you. We are here to provide personalized guidance and support to ensure your
+            project's success.
+          </p>
+
+          <GetInTouchForm onSubmitFail={() => {}} onSubmitSuccess={() => {}} />
+        </Typography>
+      </Footer>
+    </>
   );
 };
