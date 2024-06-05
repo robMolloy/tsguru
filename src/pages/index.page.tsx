cd58dc17664bed09706741038fcf3cac52114eff
robMolloy <robmolloy@hotmail.co.uk>
2024-06-05 17:32:23 +0100

    home page tidy

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,19 +1,21 @@
-import { Typography } from "@/components";
+import { Typography, VerticalSpacing } from "@/components";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
       <Typography fullPage>
-        <div className="flex flex-col text-center pt-12 sm:pt-24 pb-32 sm:pb-48 gap-8">
-          <h1 className="text-[3rem] text-primary my-0">
+        <VerticalSpacing size="big" />
+
+        <div className="flex flex-col gap-8 text-center">
+          <h1 className="my-0 text-[3rem] text-primary">
             Are you productive in the world's most productive language?
           </h1>
-          <h2 className="text-[1.5rem] mx-10 my-0 ">
+          <h2 className="mx-10 my-0 text-[1.5rem]">
             Here at TS Gurus we focus on improving your TypeScript projects and providing the tools
             and resources to improve the code your team delivers.
           </h2>
-          <p className="flex justify-center gap-4 my-0">
+          <p className="my-0 flex justify-center gap-4">
             <Link className="btn btn-primary sm:btn-wide" href="/services/audit-service">
               Audit your app
             </Link>
@@ -23,32 +25,34 @@ export default function Page() {
           </p>
         </div>
 
+        <VerticalSpacing size="bigger" />
+
+        <div className="flex justify-center">
+          <blockquote>
+            <p>become a master of TypeScript</p>
+          </blockquote>
+        </div>
+
+        <VerticalSpacing size="bigger" />
+
         <p>
           Annually,{" "}
           <a href="https://octoverse.github.com/2022/top-programming-languages">
-            more code is authored in JavaScript than any other language,
+            more code is authored to run in JavaScript than any other language,
           </a>{" "}
-          with this figure continually rising. The flexibility of JavaScript is a key benefit that
-          simplifies development, but it has its trade-offs. TypeScript can address these issues,
-          but if implemented poorly can lead to additional complications.
-        </p>
-
-        <p>
-          At TS Gurus we offer a full suite of services that can be applied to part or all of a
-          project of any size.
+          and the proportion that is written in TypeScript is growing every year. Despite this,
+          TypeScript is often seen more of a necessary inconvenience than a helpful tool.
         </p>
         <p>
-          We also provide a number of upskilling and training services to get your whole team to
-          write high quality code and following TypeScript best-practices. Unlock the full potential
-          of TypeScript with TS Gurus today by getting us to{" "}
-          <Link href="/services/audit-service"> audit your project</Link>! Or view our{" "}
+          At TS Gurus we would encourage you to stop struggling with the tools you use and set a new
+          goal - become a master of TypeScript.
+        </p>
+        <p>
+          We provide services for enhance the quality of code developers write as well as services
+          to audit and improve the code written in existing projects. Unlock your full potential of
+          TypeScript with TS Gurus today by getting us to{" "}
+          <Link href="/services/audit-service">audit your project</Link>! Or view our{" "}
           <Link href="/services">full range of services</Link>.
-        </p>
-        <p>
-          When used correctly, TypeScript will save development time due to the reduction in debug
-          time, and completely removing entire categories of errors. However, the TypeScript
-          compiler works across your whole project, and small errors in one area can cause errors
-          throughout.
         </p>
       </Typography>
     </div>
